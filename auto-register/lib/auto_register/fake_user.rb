2f9fda3469c436a7897233536ae911c3470f3a32
Victor Makoed <vmakoed@gmail.com>
2016-01-24 22:20:28 +0300

    Added time and id info when writing to .csv

--- auto-register/lib/auto_register/fake_user.rb
+++ auto-register/lib/auto_register/fake_user.rb
@@ -2,18 +2,20 @@ require 'faker'
 require 'fileutils'
 
 class FakeUser
-  attr_accessor :username, :email, :password
+  attr_accessor :username, :email, :password, :id
 
-  def initialize
+  def initialize(id = -1)
     @username = Faker::Internet.user_name
     @email = Faker::Internet.safe_email
     @password = Faker::Internet.password
+    @id = id
   end
 
   def save_to_csv(file_path)
     FileUtils.touch(file_path)
     File.open(file_path, "ab") do |csv|
-      csv << "Username:\t#{@username}\nEmail:\t#{@email}\nPassword:\t#{@password}\n\n"
+      csv << "##{@id}\nUsername:\t#{@username}\nEmail:\t#{@email}\nPassword:\t#{@password}\n"
+      csv << "Record time: #{Time.now}\n\n"
     end
   end
 end
